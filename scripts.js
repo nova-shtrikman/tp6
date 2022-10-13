b9bbe08d92309176df29dfae8636b2f2c4b018e0
Nova <nova.shtrikman@wsu.edu>
2022-10-13 19:42:33 +0000

    updated with AJAX calls and timout function

--- scripts.js
+++ scripts.js
@@ -1,7 +1,38 @@
-//Javascript for TP5
+//Javascript for TP6
+
+
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+
 
 
 window.onload = function() {
+  
+  loadFileInto("ingredients.html", "#ingreidnets ul");
+  loadFileInto("directions.html", "#directions ul");
+  loadFileInto("equiptment.html", "#equiptment ul");
   
   document.querySelector("#titleBanner h1").classList.add("tp");
   
@@ -23,5 +54,6 @@ window.onload = function() {
     
     
    document.querySelector("#recipeColomns #equiptment ul").innerHTML += "<li>An appetite for candy</li>";
+  
     
 } //end window.onload 
